d4eda85d399af589c4079dd6cdb16bf3d5ecf006
minsu <kimms5617@naver.com>
2023-03-06 20:55:54 +0900

    [Programmers] 짝지어 제거하기 - 자료구조 사용할 경우(해결)

--- src/Programmers/Sol7.java
+++ src/Programmers/Sol7.java
@@ -1,42 +1,28 @@
 package Programmers;
 
+import java.util.*;
 public class Sol7 {
-    public static void main(String[] args) {
-        String tmp = "cdcd";
-        System.out.println(solution(tmp));
-    }
-    public static int solution(String s)
+    class Solution
     {
-        int answer = 0;
+        public int solution(String s)
+        {
+            // 정답 보고 1일 뒤 시작. -> 자료구조 숙지 필요.
+            // stack을 쓰는 것이 for을 쓸때 보다 몇배 더 효율설 보장 -> 자료구조 기본 초급 중급 숙지
+            Stack<Character> stack = new Stack<>();
+            for(char tmp : s.toCharArray()){
+                if(!stack.isEmpty() && stack.peek() == tmp){
+                    stack.pop(); // 중복되는 쌍이 발견돼었기 때문에 삭제.
+                }else{
+                    stack.push(tmp); // 2개의 쌍이 없는 경우는 새로 넣어주기
+                }
+            }
 
-        int tmp = 0;
-        while(true){
-            int index = check(s);
-            if(index == -1){
-                if(s.length() == 0){
-                    answer = 1;
-                    return answer;
-                }
-                else{
-                    answer = 0;
-                    return answer;
-                }
+            if(stack.isEmpty()){ // 없는 경우 삭제.
+                return 1;
             }else{
-                s = s.substring(0, index) + s.substring(index+2, s.length());
+                return 0;
             }
-        }
 
-        // return answer;
-    }
-
-    public static int check(String s){
-        for(int i = 0; i < s.length(); i++){
-            if(i+2 <= s.length()){
-                if(s.substring(i, i+1).equals(s.substring(i+1, i+2))){
-                    return i;
-                }
-            }
         }
-        return -1;
     }
 }
