2c32fc79cfe61b3bed7cc6d59a612c805f83d664
minsu <kimms5617@naver.com>
2023-04-10 13:28:00 +0900

    [Backjoon] 알파벳 대문자

---
+++ src/HotFixStudy/simulation/Easy05.java
@@ -0,0 +1,33 @@
+package HotFixStudy.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Easy05 {
+    /**
+     * 백준 11655 ROT13
+     * 알파벳 대문자
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = bf.readLine();
+        String answer = "";
+        for(int i = 0; i< input.length(); i++){
+            if(input.charAt(i) == ' ' || (input.charAt(i) >= '0' && input.charAt(i) <= '9')){
+                answer += input.charAt(i);
+            }else {
+                if(input.charAt(i) >= 'a' && input.charAt(i) <= 'z' && (char)(input.charAt(i) + 13) > 'z'){
+                    answer += (char) ((input.charAt(i) - ('z' - 'a' + 1)) + 13 );
+                }else if(input.charAt(i) >= 'A' && input.charAt(i) <= 'Z' && (char)(input.charAt(i) + 13) > 'Z'){
+                    answer += (char) ((input.charAt(i) - ('Z' - 'A' + 1)) + 13);
+                }else {
+                    answer += (char) (input.charAt(i) + 13);
+                }
+            }
+        }
+        System.out.println(answer);
+
+    }
+}
