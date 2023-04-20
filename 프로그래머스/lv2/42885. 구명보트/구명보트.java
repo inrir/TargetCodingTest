ed2169f3840a8ac10ae836a223169117e5dba085
inrir <kimms5617@naver.com>
2023-04-20 20:29:03 +0900

    [level 2] Title: 구명보트, Time: 9.02 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.java
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -4,19 +4,28 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(people); // 오름차순
+        // 30 40 50 60 70
+        // 순차적으로 한다면, 30 + 40, 50, 60, 70
+        // 아래 로직대로면, 30 + 70, 40 + 60, 50 
+        int min = 0;
+        int max = people.length - 1;
         
-        int left = 0;
-        int right = people.length - 1;
-        
-        while(left <= right){ // 로직을 제대로 생각. -> 우선 2개만 가능하다는 점.
-            if(people[left] + people[right] <= limit){
-                left++;
+        while(min <= max){ // 로직을 제대로 생각. -> 우선 2개만 가능하다는 점.
+            if(people[min] + people[max] <= limit){
+                min++;
             }
-            right--;
+            max--;
             answer++;
         }
         
-        // 근데 왜 앞에서 순차적으로 다가갈 수 없는 걸까?
+        // // 근데 왜 앞에서 순차적으로 다가갈 수 없는 걸까?
+        // for(int i = 0 ; i < people.length; i++){
+        //     if(i+1 < people.length && people[i] + people[i + 1] <= limit){
+        //         i++;
+        //     }
+        //     answer++;
+        // }
+        
         
         return answer;
     }
