a0e19a746112e41050c6f2f9435f2adabd5da5fc
minsu <kimms5617@naver.com>
2023-04-27 18:48:32 +0900

    [Backjoon] 구현Easy 리스트

---
+++ src/HotFixStudy/simulation/Easy16.java
@@ -0,0 +1,37 @@
+package HotFixStudy.simulation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Easy16 {
+    /**
+     * 백준 11399 ATM
+     * 정렬 후 구현하면 될 거 같은 느낌?
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(bf.readLine()); // 인원 수
+
+        int[] nums = new int[num];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        for(int i = 0; i < num; i++){
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        int value = 0;
+        int sum = 0;
+        for(int i = 0; i < num; i++){
+            value += nums[i];
+            sum += value;
+        }
+
+        System.out.println(sum);
+    }
+}
