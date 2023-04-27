0a5c586c609b3f6fdb1364ab662f0d060daf6693
inrir <kimms5617@naver.com>
2023-04-27 12:46:02 +0900

    [Bronze II] Title: 손익분기점, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    /**
+     * 백준 1712 손익분기점
+     * 그저 비용 계산하면 안되나?
+     */
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] tmp = bf.readLine().split(" ");
+        int A = Integer.parseInt(tmp[0]); // fixed cost
+        int B = Integer.parseInt(tmp[1]); // variable cost
+        int C = Integer.parseInt(tmp[2]); // price
+
+        int expect = 1;
+        // A + B * expect <= C * expect 시점 찾기
+        // 언제가 안된다고 말하지??
+
+        if(B >= C){
+            System.out.println(-1);
+        }else{
+            int answer = A / (C - B);
+            answer++;
+            System.out.println(answer);
+        }
+
+
+    }
+}
