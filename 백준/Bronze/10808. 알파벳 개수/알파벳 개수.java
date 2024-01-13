f09e00c9a44bd6f1b12fea6c8eb50bd370d4708e
inrir <kimms5617@naver.com>
2024-01-13 18:19:13 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 144 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,28 +1,25 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    /**
-     * 백준 10808 알파벳 개수
-     * 브론즈 4
-     * 알파벳 배열 => 알파벳마다 넣어주는 형식으로 개수 세기
-     * 소문자만 받음
-     */
     public static void main(String[] args) throws IOException {
+        // input: string only 소문자
+        // output: split alphabet.?
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
         String input = bf.readLine();
-        int[] alpha = new int[26];
+
+        int[] a = new int[26];
 
         for(int i = 0; i < input.length(); i++){
-            alpha[(int)(input.charAt(i) - 'a')]++;
+            int tmp = input.charAt(i) - 'a';
+            a[tmp]++;
         }
 
-        for(int i = 0; i < alpha.length - 1; i++){
-            System.out.print(alpha[i] + " ");
+        for(int i = 0; i < 26; i++){ // 알파벳 총 길이 26개
+            System.out.print(a[i] + " ");
         }
-        System.out.print(alpha[alpha.length - 1]);
+
     }
 }
